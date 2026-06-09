f80019dd98c4f7398378bccb19c8e8c6982b012a
Kalin K <kalin.kuzev@city.ac.uk>
2026-06-09 14:25:57 +0100

    Various Improvements
    Added HTML files top display a visually appealing GUI as well as various functions like being able to register and login as well as creating posts on the website

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,36 +2,35 @@ import os
 from flask import Flask
 
 
-#'flask --app flask_app:instantiate_app run' to run in the Terminal
-#'flask --app flask_app:instantiate_app init-db' to initialise database
-#Go to http://127.0.0.1:5000/works to see output
-def instantiate_app(test_config=None):
+#'flask --app flask_app:create_app run' to run in the Terminal
+#'flask --app flask_app:create_app init-db' to initialise database
+#Go to http://127.0.0.1:5000/ to see output
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
     app.config.from_mapping(
-        SECRET_KEY='epic',
-        DATABASE=os.path.join(app.instance_path, 'flask_app.sqlite'),
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
     if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
-    #flask DOESN'T auto make instance folders so it must be done manually
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    @app.route('/works')
-    def hello():
-        return 'Working!'
-
-    #You can use '.' instead of 'flask_app' to import from root directory!
-    from . import db_init
-    db_init.init_app(app)
+    from . import db
+    db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
 
     return app
--- flask_app/auth.py
+++ flask_app/auth.py
@@ -1,94 +1,83 @@
 import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import generate_password_hash, check_password_hash
+from .db import get_db
 
-from flask_app.db_init import init_db
-
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
-
+def load_user():
+    user_id = session.get("user_id")
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db.execute(
-            'SELECT * FROM user WHERE id = ?',
-            (user_id,)).fetchone(
-        )
+        g.user = get_db().execute(
+            "SELECT * FROM user WHERE id = ?", (user_id,)
+        ).fetchone()
 
-
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = init_db()
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username required"
         elif not password:
-            error = 'Password is required.'
+            error = "Password required"
 
         if error is None:
             try:
                 db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
                 db.commit()
+                return redirect(url_for("auth.login"))
             except db.IntegrityError:
-                error = f'User {username} already exists.'
-            else:
-                return redirect(url_for('auth.login'))
+                error = "User already exists"
 
         flash(error)
 
-    return render_template('auth/register.html')
+    return render_template("auth/register.html")
 
-
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = init_db()
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
         error = None
 
-        if not password:
-            error = 'Password is required.'
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?",
+            (username,)
+        ).fetchone()
 
-        correct_user = db.execute(
-                'SELECT * FROM user WHERE username = ?',
-                (username,)).fetchone(
-            )
-
-        if correct_user is None or check_password_hash(correct_user['password'], password):
-            error = 'Username or Password is incorrect.'
-
-        if error is None:
+        if user is None or not check_password_hash(user["password"], password):
+            error = "Incorrect username or password"
+        else:
             session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
 
         flash(error)
-    return render_template('auth/login.html')
 
-@bp.route('/logout')
+    return render_template("auth/login.html")
+
+@bp.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
-
+    return redirect(url_for("index"))
 
 def login_required(view):
     @functools.wraps(view)
-    def wrapped_view(**kwargs):
+    def wrapped(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for("auth.login"))
         return view(**kwargs)
-    return wrapped_view
+    return wrapped
